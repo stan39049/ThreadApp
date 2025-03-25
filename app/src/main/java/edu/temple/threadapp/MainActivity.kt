90d647800f4332c88278f2726e18406aa2745add
stan39049 <stan39049@gmail.com>
2025-03-25 12:09:35 -0400

    Used handler to display countdown on display

--- app/src/main/java/edu/temple/threadapp/MainActivity.kt
+++ app/src/main/java/edu/temple/threadapp/MainActivity.kt
@@ -1,6 +1,9 @@
 package edu.temple.threadapp
 
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import android.os.Message
 import android.util.Log
 import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
@@ -12,15 +15,23 @@ class MainActivity : AppCompatActivity() {
 
     val countdownTextView: TextView by lazy { findViewById(R.id.countdownTextView) }
 
+    val countdownHandler = Handler(Looper.getMainLooper()){
+        countdownTextView.text = it.what.toString()
+
+        true
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         Thread{
             repeat(100) {
-                val countdown = (100 - it).toString()
-                Log.d("Countdown: ", countdown)
-
+                val countdown = (100 - it)
+                Log.d("Countdown: ", countdown.toString())
+                val msg = Message()
+                msg.what = countdown
+                countdownHandler.sendMessage(msg)
                 Thread.sleep(1000)
             }
         }.start()
